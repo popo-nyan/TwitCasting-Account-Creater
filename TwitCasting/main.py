c2c1c5e1fbf78ddbb4055af5158e2a4319054c67
kounomiya <kounomiya@tuta.io>
2024-02-13 17:31:44 +0900

    ヘッダーにUserIdをSessionKeyを入れるようにした

--- TwitCasting/main.py
+++ TwitCasting/main.py
@@ -5,8 +5,8 @@ from utils import authorizekey_calculating, tc_ss_calculating
 
 
 def main() -> None:
-    user_id = "nyan"
-    email_address = "hoge@example.net"
+    user_id = "jascdk31alfja"
+    email_address = "Caoge@example.net"
     password = "".join(secrets.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits + "%&$#()") for _ in range(20))
     json_data = {"user_id": user_id, "email": email_address, "name": user_id, "password": password, "lang": "en", "app_id": "TCViewerAndroid", "device_type": "android", "app_version": "5.639", "device_id": secrets.token_hex(16)}
     headers = {
@@ -29,15 +29,19 @@ def main() -> None:
     print(device_salt, session_key)
     print(tc_ss_calculating(device_salt=device_salt, session_key=session_key))
     
+    headers["Userid"] = f"c:{user_id}"
+    headers["Sessionkey"] = session_key
     headers["X-Authorizekey"] = authorizekey_calculating(http_method="POST", endpoint="/privacy_policy/agree", session_key=session_key)
-    httpx.post(
+    resp = httpx.post(
         url="https://iapi.twitcasting.tv/privacy_policy/agree",
         headers=headers)
-    headers["X-Authorizekey"] = authorizekey_calculating(http_method="POST", endpoint="/agree", payload={"over20": True}, session_key=session_key)
-    httpx.post(
+    print(resp, resp.text)
+    headers["X-Authorizekey"] = authorizekey_calculating(http_method="POST", endpoint="/verify_age", payload={"over20": True}, session_key=session_key)
+    resp = httpx.post(
         url="https://iapi.twitcasting.tv/verify_age",
         headers=headers,
         json={"over20": True})
+    print(resp, resp.text)
 
 
 if __name__ == "__main__":
